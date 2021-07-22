83365e49c31b6ed9811c9693033b8a88c32000cd
marcellpaganini <59565705+marcellpaganini@users.noreply.github.com>
2021-07-22 16:40:49 -0300

    madlibs working

--- App/app/src/main/java/com/example/madlibs/MainActivity.kt
+++ App/app/src/main/java/com/example/madlibs/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.madlibs
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Context
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -50,17 +52,46 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.btn_7).setOnClickListener {
             pluralNoun2 = go(findViewById(R.id.input_7), findViewById(R.id.input_8), findViewById<EditText>(R.id.txtPluralNoun2).text.toString())
         }
+
+        findViewById<Button>(R.id.btn_8).setOnClickListener {
+            adjective2 = showText(findViewById(R.id.input_8), findViewById(R.id.scroll), findViewById<EditText>(R.id.txtAdjective2).text.toString())
+        }
+
     }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (currentFocus != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(currentFocus!!.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
 
     private fun go(currentLayout : LinearLayout?, nextLayout : LinearLayout?, input : String) : String {
         currentLayout?.visibility = View.GONE
         nextLayout?.visibility = View.VISIBLE
 
-        var answer : String = input
+        val answer : String = input
         return answer
     }
 
-    private fun showText() {
+    private fun showText(currentLayout : LinearLayout?, scrollView : ScrollView, input : String) : String {
+        currentLayout?.visibility = View.GONE
+        scrollView.visibility = View.VISIBLE
+        findViewById<ImageView>(R.id.computer).visibility = View.VISIBLE
 
+        val answer : String = input
+
+        val text = getString(R.string.string_1) + " " + noun + getString(R.string.string_2) + " " + pluralNoun1 +
+                getString(R.string.string_3) + " " + verbPresent1 + getString(R.string.string_4) +  " " + verbPresent2 + " " +
+                getString(R.string.string_5) +  " " + bodyPart + getString(R.string.string_6) +  " " + adjective1 + " " +
+                getString(R.string.string_7) +  " " + pluralNoun2 + " " + getString(R.string.string_8) +  " " + answer +
+                " " + getString(R.string.string_9)
+        findViewById<TextView>(R.id.txtContent).text = text
+
+        return answer
     }
+
+
 }
